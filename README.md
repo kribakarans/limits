19ad5d1e4b5e7934d8adf3a96e2ef74add0094b7
Kirubakaran Shanmugam <kribakarans@gmail.com>
2022-10-17 18:50:16 +0530

    Removed signed types

--- README.md
+++ README.md
@@ -3,28 +3,26 @@ Limits of C datatype sizes and standard Linux macros limits.
 
 **Sample output: 64 bit machine**
 ```
-cc -g limits.c -o limits.out
+Bytes Type
+----- ----
  1  - char
- 1  - unsigned char
- 4  - int
- 4  - unsigned int
- 8  - long unsigned int
- 8  - unsigned long int
+
  2  - short
- 2  - unsigned short
- 4  - unsigned
  2  - short int
- 2  - unsigned short int
- 2  - short unsigned int
+
+ 4  - int
+ 4  - unsigned
+ 4  - float
+
  8  - long
- 8  - unsigned long
  8  - long int
- 8  - unsigned long int
  8  - long long int
- 8  - unsigned long long int
+ 8  - unsigned long
+
  8  - double
+
  16 - long double
- 8  - size_t
+
  1  - uint8_t
  1  - u_int8_t
  2  - uint16_t
@@ -33,7 +31,9 @@ cc -g limits.c -o limits.out
  4  - u_int32_t
  8  - uint64_t
  8  - u_int64_t
- 4  - float
+ 8  - size_t
+ 8  - ssize_t
+
  255  - MAX_INPUT
  255  - NAME_MAX
  255  - MAXNAMLEN
--- limits.c
+++ limits.c
@@ -10,42 +10,54 @@
 
 int main()
 {
+	printf("\nBytes Type");
+	printf("\n----- ----\n");
+
+	/* 1 byte */
 	print_size(char);
-	print_size(unsigned char);
+	//print_size(unsigned char);
+	puts("");
 
-	print_size(int);
-	print_size(unsigned int);
-	print_size(long unsigned int);
-	print_size(unsigned long int);
-
+	/* 2 byte */
 	print_size(short);
-	print_size(unsigned short);
-
-	print_size(unsigned);
-
 	print_size(short int);
-	print_size(unsigned short int);
-	print_size(short unsigned int);
+	//print_size(unsigned short);
+	//print_size(unsigned short int);
+	//print_size(short unsigned int);
+	puts("");
 
-	//print_size(short short int); /* ERROR: duplicate 'short' */
+	/* 4 byte */
+	print_size(int);
+	print_size(unsigned);
+	//print_size(unsigned int);
+	print_size(float);
+	//print_size(long float);  /* ERROR: both ‘long’ and ‘float’ in declaration specifiers */
+	//print_size(short float); /* ERROR: both ‘short’ and ‘float’ in declaration specifiers */
+	//print_size(unsigned float); /* ERROR: both ‘unsigned’ and ‘float’ in declaration specifiers */
+	puts("");
 
+	/* 8 byte */
 	print_size(long);
-	print_size(unsigned long);
-
 	print_size(long int);
-	print_size(unsigned long int);
-
 	print_size(long long int);
-	print_size(unsigned long long int);
-
+	print_size(unsigned long);
+	//print_size(unsigned long int);
+	//print_size(long unsigned int);
+	//print_size(unsigned long long int);
+	//print_size(short short int); /* ERROR: duplicate 'short' */
 	//print_size(long long long int); /* ERROR: ‘long long long’ is too long for GCC */
+	puts("");
 
+	/* 8 byte */
 	print_size(double);
+	puts("");
+
+	/* 16 byte */
 	print_size(long double);
 	//print_size(long long double); /* ERROR: both ‘long long’ and ‘double’ in declaration specifiers */
 	//print_size(double double); /* ERROR: two or more data types in declaration specifiers */
+	puts("");
 
-	print_size(size_t);
 	print_size(uint8_t);
 	print_size(u_int8_t);
 	print_size(uint16_t);
@@ -54,11 +66,9 @@ int main()
 	print_size(u_int32_t);
 	print_size(uint64_t);
 	print_size(u_int64_t);
-
-	print_size(float);
-	//print_size(long float);  /* ERROR: both ‘long’ and ‘float’ in declaration specifiers */
-	//print_size(short float); /* ERROR: both ‘short’ and ‘float’ in declaration specifiers */
-	//print_size(unsigned float); /* ERROR: both ‘unsigned’ and ‘float’ in declaration specifiers */
+	print_size(size_t);
+	print_size(ssize_t);
+	puts("");
 
 	print_limit(MAX_INPUT);
 	print_limit(NAME_MAX);
@@ -75,36 +85,37 @@ int main()
  Output: Compiled and Ran in 64 bit Linux machine
  cc -g limits.c -o limits.out
 
- 1 - char
- 1 - unsigned char
- 4 - int
- 4 - unsigned int
- 8 - long unsigned int
- 8 - unsigned long int
- 2 - short
- 2 - unsigned short
- 4 - unsigned
- 2 - short int
- 2 - unsigned short int
- 2 - short unsigned int
- 8 - long
- 8 - unsigned long
- 8 - long int
- 8 - unsigned long int
- 8 - long long int
- 8 - unsigned long long int
- 8 - double
+Bytes Type
+----- ----
+ 1  - char
+
+ 2  - short
+ 2  - short int
+
+ 4  - int
+ 4  - unsigned
+ 4  - float
+
+ 8  - long
+ 8  - long int
+ 8  - long long int
+ 8  - unsigned long
+
+ 8  - double
+
  16 - long double
- 8 - size_t
- 1 - uint8_t
- 1 - u_int8_t
- 2 - uint16_t
- 2 - u_int16_t
- 4 - uint32_t
- 4 - u_int32_t
- 8 - uint64_t
- 8 - u_int64_t
- 4 - float
+
+ 1  - uint8_t
+ 1  - u_int8_t
+ 2  - uint16_t
+ 2  - u_int16_t
+ 4  - uint32_t
+ 4  - u_int32_t
+ 8  - uint64_t
+ 8  - u_int64_t
+ 8  - size_t
+ 8  - ssize_t
+
  255  - MAX_INPUT
  255  - NAME_MAX
  255  - MAXNAMLEN
